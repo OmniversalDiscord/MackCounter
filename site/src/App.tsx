0c795fb461112a2f7328afa18c27d32795e82b7b
fluxehub <53355311+fluxehub@users.noreply.github.com>
2022-10-11 01:36:16 +0100

    Front-end

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,7 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import CountUp from "react-countup";
+import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [count, setCount] = useState<number | null>(null);
+
+  useEffect(() => {
+    fetch(".netlify/functions/getCount").then((res) => {
+      res.text().then((c) => {
+        setCount(parseInt(c));
+      });
+    });
+  }, []);
+
+  if (count === null) {
+    return <div className="App"></div>;
+  }
+
+  return (
+    <div className="App">
+      <CountUp className="count" end={count} duration={1.5} useEasing={true} />
+      <p className="subtitle">"oh wait I gotta go eat" moments</p>
+      <p className="date">since October 9th, 2022</p>
+      <button
+        onClick={() => {
+          fetch(".netlify/functions/addInstance", { method: "POST" }).then(() =>
+            setCount(count + 1)
+          );
+        }}
+      >
+        Add moment
+      </button>
+    </div>
+  );
 }
 
 export default App;
